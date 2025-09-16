1c7f307b505dff9ad0be65b4e13bd5b4b7814887
dungstergen <nguyentiendung01062005@gmail.com>
2025-09-16 16:38:55 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Frontend.index');
+});
+
+Route::get('/about', function () {
+    return view('Frontend.about');
+});
+
+Route::get('/pricing', function () {
+    return view('Frontend.pricing');
+});
+
+Route::get('/why', function () {
+    return view('Frontend.why');
+});
+
+Route::get('/testimonial', function () {
+    return view('Frontend.testimonial');
 });
